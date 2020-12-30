793370e99af15365a5b275ea49aea658605f3744
Jon Shipman <jon@jonshipman.com>
2020-12-30 16:24:31 -0600

    dev min false

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,9 @@ const common = require("./webpack.common.js");
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
+  optimization: {
+    minimize: false,
+  },
   devServer: {
     contentBase: "./dist",
   },
